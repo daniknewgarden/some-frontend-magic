1f35084ba7d1c25501a489610cf220fc77d88cc3
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-02-06 20:09:32 +0100

    feat(step 2): implement blocks adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { InputArea } from "./components";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/base";
+
+interface Block {
+  id: string;
+}
 
 function App() {
-  return <InputArea />;
+  const [blocks, setBlocks] = useState<Block[]>([]);
+
+  const onBlockAdd = () => {
+    setBlocks(prev => {
+      return [...prev, { id: uuidv4() }];
+    });
+  };
+
+  return (
+    <Stack spacing={2} justifyContent="center">
+      {blocks.map(block => (
+        <InputArea key={block.id} />
+      ))}
+      <Button onClick={onBlockAdd}>Add Block</Button>
+    </Stack>
+  );
 }
 
 export default App;
