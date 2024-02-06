72e8486c362fca7b84ac5327e03b82c72b654481
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-02-06 20:31:35 +0100

    feat(step 3): add code evaluation

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -1,12 +1,45 @@
 import { TextareaAutosize } from "@mui/base/TextareaAutosize";
 import { Button } from "@mui/base/Button";
 import Stack from "@mui/material/Stack";
+import { Box } from "@mui/material";
+import { FormEvent, useRef, useState } from "react";
 
+// TODO: rename to CodeBlock
 export function InputArea() {
+  const [executionResult, setExecutionResult] = useState("jfksdjflk");
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const value = inputRef.current?.value;
+
+    if (!value) {
+      setExecutionResult("No value");
+    }
+
+    try {
+      const result = eval(JSON.stringify(value));
+      setExecutionResult(result.toString());
+    } catch (error: any) {
+      setExecutionResult(error.message);
+    }
+  };
+
   return (
-    <Stack direction="row" spacing={2} alignItems="flex-start">
-      <TextareaAutosize minRows={3} aria-label="empty textarea" placeholder="Empty" />
-      <Button>Button</Button>
+    <Stack direction="column" spacing={2} alignItems="flex-start">
+      <Stack direction="row" spacing={2} alignItems="flex-start">
+        <form onSubmit={onSubmit}>
+          <TextareaAutosize
+            minRows={3}
+            aria-label="empty textarea"
+            placeholder="Empty"
+            id="value"
+            ref={inputRef}
+          />
+          <Button type="submit">Run</Button>
+        </form>
+      </Stack>
+      <Box>{executionResult}</Box>
     </Stack>
   );
 }
